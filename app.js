4d39c47b5be75607f382aee4bfeae2e4fed56a57
Thirumavalavan <catchgjt@gmail.com>
2016-12-14 17:27:34 +0530

    inital move

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://catchgjt:root@ds037015.mongolab.com:37015/thirun_employee';
+
+mongoose.connect(dbURI);
+var testSchema = new mongoose.Schema({
+        name: String,
+        email: String,
+		DOB:String,
+		department:String,
+		gender:String
+});
+
+var users = mongoose.model('users', testSchema);
+
+app.get('/users', function(req, res){
+
+        users.find({},function(err, user){
+                 if (err) throw err;
+		res.send(user);
+        });
+});
+app.post('/users', function(req, res, next) {
+   var use = new users({
+      name: req.body.name,
+      email: req.body.email,
+	  DOB : req.body.DOB,
+	  department:req.body.department,
+	  gender:req.body.gender
+   });
+   console.log(use);
+   use.save(function(err, data) {
+      if(err) {
+         return next(err);
+      }
+      res.status(201).json(data);
+   });
+});
+app.delete('/users/:id', function(req, res) {
+   users.findByIdAndRemove(req.params.id, function(err, user) {
+      res.json(user);
+   });
+});
+app.get("/users/:id", function(req,res) {
+	console.log('Received findOne person request');
+	console.log(req.params.id);
+	users.findById(req.params.id, function(err, user){
+		console.log(user);
+	});
+});
+
+app.put('/users/:id', function(req,res){
+users.findByIdAndUpdate(req.params.id, function(err,user){	
+			  user.name = req.body.name;
+              user.email = req.body.email;
+			  user.DOB = req.body.DOB;
+			  user.department = req.body.department;
+			  user.gender = req.body.gender;
+			  user.save (function(err,user){
+			  if (err) throw err;
+			  else{
+			  res.send(user);
+			  }
+			  });
+			  });
+});
+
+
+app.get('/', function (req, res) {
+res.sendFile('/index.html' , { root : __dirname});
+});
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
---
+++ js/controller.js
@@ -0,0 +1,46 @@
+var myApp = angular.module('myApp', []);
+myApp.controller("AppCtrl", ['$scope', '$http', function($scope, $http) {
+
+  var updateView=function(){
+         $http.get('/users').success(function(response) {
+         $scope.users = response; 
+         $scope.user="";
+        });
+  };
+    $scope.editUsers = function(id) {
+      $http.get('/users/' + id).success(function(data) {
+      $scope.user = data;
+     });
+   };
+  updateView();
+  
+    $scope.addUsers = function() {
+      $http.post('/users', $scope.users).success(function(response) {
+        updateView();
+      });
+    };
+    
+    $scope.removeUsers = function(id) {
+      $http.delete('/users/' + id).success(function(response) {
+          updateView();
+      });
+    };
+    
+
+    
+    $scope.updateUsers = function() {
+     console.log($scope.users._id);
+     $http.put('/users/' + $scope.users._id, $scope.name).success(function(response) {
+       updateView();
+     });
+    };
+    
+$scope.calculateAge = function(DOB) { // pass in player.dateOfBirth
+    var ageDifMs = Date.now() - new Date(DOB);
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+}]);
+
+
+
